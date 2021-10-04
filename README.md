c71273f2c89eeecb53efaf437f1814d01ccd067d
parajuliamit <be2017se587@gces.edu.np>
2021-10-04 11:00:38 +0545

    project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# nepaldometer
+
+Nepal covid tracker.
---
+++ android/app/src/main/kotlin/com/urjalab/covidnepal/nepaldometer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.urjalab.covidnepal.nepaldometer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
